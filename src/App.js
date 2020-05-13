48d9e2abb68a24bf7eecaddb717355539d755eb8
E Ching Kho <echingkho@E-Chings-MacBook-Pro-15-2018.local>
2020-05-13 15:22:24 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Today is Tuesday! Good Evening! it is currently 5:30 pm
         </p>
         <a
           className="App-link"
